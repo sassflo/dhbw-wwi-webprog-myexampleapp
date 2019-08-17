95d05f2a2f206cc18b71c2bdaae710814f8bedfa
Florian Saß <florian.sass@online.de>
2019-08-17 19:08:15 +0200

    Added Under Construction Site

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@
 //JS File for the Main Window
 
 import App from "./app.js";
-import stylesheet from "./index.css"
+import stylesheet from "./index.css";
+import theme from "./theme.css";
 
 //When DOM ready
 window.addEventListener("load", () => {
