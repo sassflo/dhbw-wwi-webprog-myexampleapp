07c52555d36eaaf101f2ebc1e8510fd78da7cd50
Florian Saß <florian.sass@online.de>
2019-09-16 08:23:44 +0200

    Fixed Router

--- src/my-receipes/my-receipes.js
+++ src/my-receipes/my-receipes.js
@@ -3,9 +3,12 @@
 import stylesheet from "./my-receipes.css";
 import DB from "../database.js";
 
+let _app = "";
+
 class MyReceipes {
   constructor(app) {
     this._app = app;
+    _app = this._app;
   }
 
   onShow() {
@@ -15,29 +18,8 @@ class MyReceipes {
     let db = new DB();
     db.getAllReceipes().then(function(querySnapshot)
     {
-      let list = document.getElementById("receipe-list");
-      let table = document.getElementById("receipe-table");
-
-      querySnapshot.forEach(function(doc) {
-        // doc.data() is never undefined for query doc snapshots
-        console.log(doc.id, " => ", doc.data().Name);
-
-        let receipe = document.createElement("th");
-        let receipeName = document.createElement("td");
-        let receipeLink = document.createElement("td");
-
-        receipeName.classList.add("name");
-        receipeLink.classList.add("edit-link");
-
-        receipeName.textContent = doc.data().Name;
-        receipeLink.innerHTML = '<a href="/receipe/edit/' +  doc.id + '"  data-navigo>Edit</a>';
-
-        receipeLink.setAttribute("href", "/" + doc.id + "/edit");
-
-        receipe.appendChild(receipeName);
-        receipe.appendChild(receipeLink);
-        table.appendChild(receipe);
-        });
+      console.log("Receipes loaded");
+      onFinishedLoading(querySnapshot);
     });
 
     return {
@@ -55,5 +37,33 @@ class MyReceipes {
     return "Mein Rezepte";
   }
 }
+
+let onFinishedLoading = (receipes) =>
+  {
+    receipes.forEach(function(doc) {
+      let list = document.getElementById("receipe-list");
+      let table = document.getElementById("receipe-table");
+      // doc.data() is never undefined for query doc snapshots
+      console.log(doc.id, " => ", doc.data().Name);
+
+      let receipe = document.createElement("tr");
+      let receipeName = document.createElement("td");
+      let receipeLink = document.createElement("td");
+
+      receipeName.classList.add("name");
+      receipeLink.classList.add("edit-link");
+
+      receipeName.textContent = doc.data().Name;
+      receipeLink.innerHTML = '<a href="/receipe/edit/' +  doc.id + '"  data-navigo><img class="icon" src="/eye32.5ddd716b.png"><img class="icon" src="/edit32.3ab90f6c.png"></a>';
+
+      receipeLink.setAttribute("href", "/" + doc.id + "/edit");
+
+      receipe.appendChild(receipeName);
+      receipe.appendChild(receipeLink);
+      table.appendChild(receipe);
+      });
+      _app._router.updatePageLinks();
+      console.log("Alle Page Links updated");
+  }
 
 export default MyReceipes;
